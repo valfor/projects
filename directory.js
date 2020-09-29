5be3ab0d540268261b275f81bcb0824c9e7d4507
valfor <62493505+valfor@users.noreply.github.com>
2020-09-28 20:24:53 -0500

    Update directory.js

--- directory.js
+++ directory.js
@@ -35,21 +35,21 @@ function directory() {
       Http1.send();
       Http1.onreadystatechange = (e) => {uText = Http.responseText};
       setTimeout(function(){ 
-      discord = /Discord Handle:\s*<\/td>\s*<td>\s*([A-z0-9]+#[0-9]+)/.exec(uText)[1];
-      mainnation = /Main Nation:\s*<\/td>\s*<td>\s*(England|Scotland|Wales|Northern Ireland)/.exec(uText)[1];
-      if(mainnation){cnations[mainnation] += 1;} else {cnations[groupText[groups[i]]] += 1;};
-      nsnation = /Nation in NSUK:\s*<\/td>\s*<td>\s*(\w.*)/.exec(uText)[1].replace(" ","_").toLowerCase();
+      discord = /Discord Handle:\s*<\/td>\s*<td>\s*([A-z0-9]+#[0-9]+)/.exec(uText);
+      mainnation = /Main Nation:\s*<\/td>\s*<td>\s*(England|Scotland|Wales|Northern Ireland)/.exec(uText);
+      if(mainnation){cnations[mainnation[1]] += 1;} else {cnations[groupText[groups[i]]] += 1;};
+      nsnation = /Nation in NSUK:\s*<\/td>\s*<td>\s*(\w.*)/.exec(uText)
       s = "[b]Name:[/b] " + uname + "\n";
-      if(nsnation) {s += "[b]NS Nation in UK:[/b] [a href='https://nationstates.net/" + nsnation + "']" + nsnation + "\n"} else {s += s += "[b]NS Nation in UK:[/b] N/A\n"};
-      if(discord) {s += "[b]Discord:[/b] " + discord + "\n"} else {s += "[b]Discord:[/b] N/A\n"};
-      if(mainnation) {s += "[b]Constituent Nation:[/b] " + mainnation + "\n"} else {s += "[b]Constituent Nation:[/b] N/A\n"};
-      if(nsnation in listOfNations) {s += "[b][font color='green']Verified[/color][/b]"} else {s += "[b][font color='red']Nation CTE[/color][/b]"};
-      post += s + "\n";}, 1500);
+      if(nsnation) {nsnation = nsnation[1].replace(" ","_").toLowerCase(); s += "[b]NS Nation in UK:[/b] [a href='https://nationstates.net/" + nsnation + "']" + nsnation + "\n"} else {s += s += "[b]NS Nation in UK:[/b] N/A\n"};
+      if(discord) {s += "[b]Discord:[/b] " + discord[1] + "\n"} else {s += "[b]Discord:[/b] N/A\n"};
+      if(mainnation) {s += "[b]Constituent Nation:[/b] " + mainnation[1] + "\n"} else {s += "[b]Constituent Nation:[/b] N/A\n"};
+      if(nsnation && (nsnation in listOfNations)) {s += "[b][font color='green']Verified[/color][/b]"} else {s += "[b][font color='red']Nation CTE[/color][/b]"};
+      post += s + "\n";}, 2000);
       } else {continue}
     }
-  } , 1500);
-  sums = cnations["England"] + cnations["Scotland"] + cnations["Wales"] + cnations["NorthernIreland"];
-  totals = '[hr][hr][div style="text-align:center;"][b][font size="5"]United Kingdom Citizenship Directory[/font][/b][/div][hr style="text-align:center;"][hr][font size="4"][div style="text-align:center;"]current forums population:'+sums+'[/div][/font][hr][hr][div align="center"][table][tbody][tr][td style="padding:3px;"][font color="d80000" size="5"]England[/font][/td][td style="padding:3px;"][font color="373e8a" size="5"]Scotland[/font][/td][td style="padding:3px;"][font size="5" color="a8a800"]Wales[/font][/td][td style="padding:3px;"][font color="25791a" size="5"]Northern Ireland[/font][/td][/tr][tr][td style="padding:3px;"][div align="center"][font size="7"][b]'+cnations["England"]+'[/b][/font][/div][/td][td style="padding:3px;"][div align="center"][font size="7"][b]'+cnations["Scotland"]+'[/b][/font][/div][/td][td style="padding:3px;"][div align="center"][font size="7"][b]'+cnations["Wales"]+'[/b][/font][/div][/td][td style="padding:3px;"][div align="center"][font size="7"][b]'+cnations["Northern Ireland"]+'[/b][/font][/div][/td][/tr][/tbody][/table][/div]\n\n'
+  } , 2000);
+  sumN = cnations["England"] + cnations["Scotland"] + cnations["Wales"] + cnations["NorthernIreland"];
+  totals = '[hr][hr][div style="text-align:center;"][b][font size="5"]United Kingdom Citizenship Directory[/font][/b][/div][hr style="text-align:center;"][hr][font size="4"][div style="text-align:center;"]current forums population:'+sumN+'[/div][/font][hr][hr][div align="center"][table][tbody][tr][td style="padding:3px;"][font color="d80000" size="5"]England[/font][/td][td style="padding:3px;"][font color="373e8a" size="5"]Scotland[/font][/td][td style="padding:3px;"][font size="5" color="a8a800"]Wales[/font][/td][td style="padding:3px;"][font color="25791a" size="5"]Northern Ireland[/font][/td][/tr][tr][td style="padding:3px;"][div align="center"][font size="7"][b]'+cnations["England"]+'[/b][/font][/div][/td][td style="padding:3px;"][div align="center"][font size="7"][b]'+cnations["Scotland"]+'[/b][/font][/div][/td][td style="padding:3px;"][div align="center"][font size="7"][b]'+cnations["Wales"]+'[/b][/font][/div][/td][td style="padding:3px;"][div align="center"][font size="7"][b]'+cnations["Northern Ireland"]+'[/b][/font][/div][/td][/tr][/tbody][/table][/div]\n\n'
   console.log(totals + post);
 }
 }
